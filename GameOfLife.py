a6e054677b143549c4f95acd67376825f886f2fa
@NekkittAY <37750010+NekkittAY@users.noreply.github.com>
2021-07-04 15:40:10 +0300

    Update GameOfLife.py

--- GameOfLife.py
+++ GameOfLife.py
@@ -1,51 +1,65 @@
-from collections import namedtuple, defaultdict
 import time
 
-Cell = namedtuple("Cell", ["x","y"])
+def gen_world(board):
+    world = ""
+    for i in board:
+        if i == "/":
+            world += "\n"
+        else:
+            world += i 
+    return world 
     
-def neighbours(cell):
-    for x in range(cell.x-1, cell.x+2):
-        for y in range(cell.y-1, cell.y+2):
-            if (x, y) != (cell.x, cell.y):
-                yield Cell(x, y)
+def make_world(board):
+    world = ""
+    for i in range(0,len(board)):
+        if not(i == 0) and i%8==0:
+            world += "/"
+        world += board[i]
+    return world
     
-def update_neighbours(board):
-    update_neighbours = defaultdict(int)
-    for cell in board:
-        for neighbour in neighbours(cell):
-            update_neighbours[neighbour] += 1
-    return update_neighbours
+def neighbours(board, elem):
+    num=0 
+    #n = pos[i-9]+pos[i-8]+pos[i-7]+pos[i-1]+pos[i+1]+pos[i+7]+pos[i+8]+pos[i+9]
+    for j in range(7,10):
+        if elem<=len(board)-1-j:
+            if board[elem+j] == "1":
+                num+=1 
+        if elem>=0+j:
+            if board[elem-j] == "1":
+                num+=1 
+    if elem>0:
+        if board[elem-1] == "1":
+            num+=1 
+    if elem<len(board)-1:
+        if board[elem+1] == "1":
+            num+=1
+    return num
     
-def create_board(desc):
-    board = set()
-    for row, line in enumerate(desc.split("\n")):
-        for col, elem in enumerate(line):
-            if elem == "1":
-                board.add(Cell(int(col), int(row)))
-    return board
-        
-def update_board(board):
-    new_board = set()
-    for cell, count in update_neighbours(board).items():
-        if count == 3 or (cell in board and count == 2):
-            new_board.add(cell)
-    return new_board        
-                
-def BoardToStr(board, n=0):
-    if not board:
-        return "end"
-    board_str = ""
-    xs = [x for (x,y) in board]
-    ys = [y for (x,y) in board]
-    for y in range(min(ys)-n, max(ys)+1+n):
-        for x in range(min(xs)-n, max(xs)+1+n):
-            board_str += "1" if Cell(x,y) in board else "0"
-        board_str += "\n"
-    return board_str.strip()
     
-if __name__ == "__main__":
-    gen = create_board("0000110\n1110010\n0000100\n0001000")
-    for i in range(100):
-        gen = update_board(gen)
-        print(BoardToStr(gen, 2))
-        time.sleep(0.1)
+def update_world(board):
+    world = board
+    for i in range(0,len(board)):
+        n=neighbours(board,i)
+        if board[i] == "0":
+            if n==3:
+                try:
+                    world = world[:i] + "1" + world[i+1:]
+                except:
+                    world = "1" + world[i+1:]
+        elif board[i] == "1":
+            if n<2 or n>3:
+                try:
+                    world = world[:i] + "0" + world[i+1:]
+                except:
+                    world = "0" + world[i+1:]
+    return world
+
+start = input()
+while True:
+    res = update_world(start)
+    world = make_world(res)
+    gen = gen_world(world)
+    print("")
+    print(gen)
+    start = res
+    time.sleep(1)
